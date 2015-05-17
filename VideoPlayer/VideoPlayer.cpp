3f87b3dc90689d6c7332bd6ec4e9317012e080d3
yjaelex <yjaelex@163.com>
2015-05-17 12:34:38 +0800

    video player

--- VideoPlayer/VideoPlayer.cpp
+++ VideoPlayer/VideoPlayer.cpp
@@ -1,17 +1,71 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define SDL_MAIN_HANDLED
 #include <SDL.h>
+
 #include <os.h>
 
-int main(int, char**){
+//Screen attributes
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int, char**)
+{
+    SDL_Surface *screen = NULL;
+    SDL_Surface *image = NULL;
+
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         osLog(LOG_ERROR, "SDL_Init Error: %s. \n", SDL_GetError());
 		return 1;
 	}
 
+    SDL_Window *window = SDL_CreateWindow("Video Player", SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL)
+    {
+        osLog(LOG_ERROR, "CreateWindow");
+        SDL_Quit();
+        return 1;
+    }
 
+    screen = SDL_GetWindowSurface(window);
+    image = SDL_LoadBMP("background.bmp"); // loads image
+
+    //Our event structure
+    SDL_Event e;
+    //For tracking if we want to quit
+    bool quit = false;
+    while (!quit)
+    {
+        //Read any events that occured, for now we'll just quit if any event occurs
+        while (SDL_PollEvent(&e))
+        {
+            //If user closes the window
+            if (e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+            //If user presses any key
+            if (e.type == SDL_KEYDOWN)
+            {
+                quit = true;
+            }
+            //If user clicks the mouse
+            if (e.type == SDL_MOUSEBUTTONDOWN)
+            {
+                quit = true;
+            }
+        }
+        //Rendering
+        SDL_BlitSurface(image, NULL, screen, NULL);
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(30);
+    }
+
+    SDL_FreeSurface(image);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
